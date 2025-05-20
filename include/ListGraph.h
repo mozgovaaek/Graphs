aa9f9ca6c6e207ba5417fdae8e92b0a9f04fa913
Мозговая Екатерина <sashamozg2010@gmail.com>
2025-05-20 11:40:28 +0300

    Добавлен граф на списках смежности

---
+++ include/ListGraph.h
@@ -0,0 +1,33 @@
+#ifndef IGRAPH_LISTGRAPH_H
+#define IGRAPH_LISTGRAPH_H
+
+#include "IGraph.h"
+#include <iostream>
+#include <deque>
+#include <cassert>
+#include <vector>
+
+class ListGraph : public IGraph {
+public:
+    explicit ListGraph(int size);
+
+    explicit ListGraph(const IGraph& graph);
+
+    ~ListGraph() = default;
+
+    void AddEdge(int from, int to) override;
+
+    size_t VerticesCount() const override;
+
+    std::vector<int> GetNextVertices(int vertex) const override;
+
+    std::vector<int> GetPrevVertices(int vertex) const override;
+
+    void Display() const noexcept override;
+
+private:
+    std::vector<std::vector<int>> adjacencyLists;
+};
+
+
+#endif //IGRAPH_LISTGRAPH_H
